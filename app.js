e205ac65c1d8fb4d23ddf6e56bb29fefcdcd676d
Diego Augusto <professor.diegosilva@gmail.com>
2019-07-18 07:44:38 -0300

    add nodemon and change example

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware!");
-  next();
+const users = {
+  maria: {
+    name: "Maria",
+    age: "25"
+  },
+  jose: {
+    name: "Jose",
+    age: "31"
+  }
+};
+
+app.use("/users", (req, res, next) => {
+  res.setHeader("Content-type", "application/json");
+  res.send(JSON.stringify(users));
 });
 
-app.use((req, res, next) => {
-  console.log("another middleware!");
-  res.send("Ok");
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to the Express.js</h1>");
 });
 
 const server = http.createServer(app);
